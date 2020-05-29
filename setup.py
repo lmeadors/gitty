841e01ca230df78e1cb791239ff3f78ac2d5b24e
Larry Meadors <larry.meadors@gmail.com>
2020-05-29 16:42:36 -0600

    "bumped version to 1.1.0"

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gitty-lmeadors",
-    version="1.0.0",
+    version="1.1.0",
     author="Larry Meadors",
     author_email="larry.meadors@elm-software.com",
     description="An alternate git workflow tool",
