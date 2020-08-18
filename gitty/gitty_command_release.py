4132e82d696d6045022463c2b87205077aa72d63
Larry Meadors <larry.meadors@dillonsoftware.com>
2020-08-18 17:35:37 -0600

    refactor: migrate release command to use steps

--- gitty/gitty_command_release.py
+++ gitty/gitty_command_release.py
@@ -1,10 +1,55 @@
-from gitty import GittyCommand
+from gitty import GittyCommand, CommentStep, GitCommandStep, BumpVersionStep, GitCommandBump
 
 
 class GittyRelease(GittyCommand):
     _title = 'release'
     _name = 'snap a new release from here'
     _bindings = ['r', 'release']
+    _steps_from_point = [
+
+        CommentStep('{} - {}'.format(_name, 'this is already in a stabilization ecosystem'), []),
+
+        GitCommandStep('git checkout %s', ['current_release_branch']),
+        GitCommandStep('git merge %s', ['current_branch']),
+
+        BumpVersionStep('release_version'),
+        GitCommandStep('git add %s', ['project_file']),
+        GitCommandBump('release_version'),
+        GitCommandStep('git tag %s', ['release_version']),
+        GitCommandStep('git checkout %s', ['current_branch']),
+        GitCommandStep('git merge %s', ['current_release_branch']),
+
+        BumpVersionStep('next_stable_version'),
+        GitCommandStep('git add %s', ['project_file']),
+        GitCommandBump('next_stable_version'),
+
+    ]
+
+    _steps_from_master = [
+
+        CommentStep('{} - {}'.format(_name, 'this will create a new stabilization ecosystem'), []),
+
+        GitCommandStep('git checkout -b %s', ['new_stabilization_branch']),
+        GitCommandStep('git checkout -b %s', ['new_release_branch']),
+
+        BumpVersionStep('release_version'),
+        GitCommandStep('git add %s', ['project_file']),
+        GitCommandBump('release_version'),
+        GitCommandStep('git tag %s', ['release_version']),
+
+        GitCommandStep('git checkout %s', ['new_stabilization_branch']),
+        GitCommandStep('$ git merge --strategy=ours %s', ['new_release_branch']),
+        BumpVersionStep('next_stable_version'),
+        GitCommandStep('git add %s', ['project_file']),
+        GitCommandBump('next_stable_version'),
+
+        GitCommandStep('git checkout %s', ['parent_version_branch']),
+        GitCommandStep('$ git merge --strategy=ours %s', ['new_stabilization_branch']),
+        BumpVersionStep('next_master_version'),
+        GitCommandStep('git add %s', ['project_file']),
+        GitCommandBump('next_master_version'),
+
+    ]
 
     def is_available(self, context):
         if context['project_type_name'] == 'unknown':
@@ -14,126 +59,24 @@ class GittyRelease(GittyCommand):
         return not context['on_a_task']
 
     def get_description(self, context):
-        point_release = len(context['branch_parts']) > 1
 
-        steps = []
-
-        if point_release:
-            # this is a point release
-            steps += [
-                '{} - {}'.format(self._name, 'this is already in a stabilization ecosystem'),
-                '$ git checkout {}'.format(context['current_release_branch']),
-                '$ git merge {}'.format(context['current_branch']),
-                '  (bump version to {} in {} on {} branch)'.format(
-                    context['release_version'], context['project_file'], context['current_release_branch']
-                ),
-                '$ git add {}'.format(context['project_file']),
-                '$ git commit -m "bumped version to {}"'.format(context['release_version']),
-                '$ git tag {}'.format(context['release_version']),
-                '$ git checkout {}'.format(context['current_branch']),
-                '$ git merge {}'.format(context['current_release_branch']),
-                '  (bump version to {} in {} on {} branch)'.format(
-                    context['next_stable_version'], context['project_file'], context['current_branch']
-                ),
-                '$ git add {}'.format(context['project_file']),
-                '$ git commit -m "bumped version to {}"'.format(context['next_stable_version']),
-            ]
+        if len(context['branch_parts']) > 1:
+            steps = self._steps_from_point
         else:
-            # this is a new release from master - there are extra steps here
-            steps += [
-                '{} - {}'.format(self._name, 'this will create a new stabilization ecosystem'),
-                '$ git checkout -b ' + context['new_stabilization_branch'],
-                '$ git checkout -b ' + context['new_release_branch'],
-                '  (bump version to {} in {} on {} branch)'.format(
-                    context['release_version'], context['project_file'], context['new_release_branch']
-                ),
-                '$ git add {}'.format(context['project_file']),
-                '$ git commit -m "bumped version to {}"'.format(context['release_version']),
-                '$ git tag {}'.format(context['release_version']),
-                '$ git checkout {}'.format(context['new_stabilization_branch']),
-                # this is a transient branch/merge, so we won't actually merge, we'll just mark it as merged
-                '$ git merge --strategy=ours {}'.format(context['new_release_branch']),
-                '  (bump version to {} in {} on {} branch)'.format(
-                    context['next_stable_version'], context['project_file'], context['new_stabilization_branch']
-                ),
-                '$ git add {}'.format(context['project_file']),
-                '$ git commit -m "bumped version to {}"'.format(context['next_stable_version']),
-                '$ git checkout master',
-                '$ git merge --strategy=ours {}'.format(context['new_stabilization_branch']),
-                '  (bump version to {} in {} on {} branch)'.format(
-                    context['next_master_version'], context['project_file'], 'master'
-                ),
-                '$ git add {}'.format(context['project_file']),
-                # this has spaces in a parameter, so it's different...
-                '$ git commit -m "bumped version to {}"'.format(context['next_master_version'])
-            ]
+            steps = self._steps_from_master
 
-        return steps
+        description = []
+        for step in steps:
+            description += step.describe(context)
+
+        return description
 
     def do_it(self, context):
+
         if len(context['branch_parts']) > 1:
-            self.release_from_point(context)
+            steps = self._steps_from_point
         else:
-            self.release_from_master(context)
+            steps = self._steps_from_master
 
-    def release_from_master(self, context):
-        self.execute_command(context, ('git checkout -b ' + context['new_stabilization_branch']).split())
-        self.execute_command(context, ('git checkout -b ' + context['new_release_branch']).split())
-        self.bump_version_to(context, context['release_version'])
-        self.execute_command(context, 'git add {}'.format(context['project_file']).split())
-        # this has spaces in a parameter, so it's different...
-        self.execute_command(context, [
-            'git',
-            'commit',
-            '-m',
-            '"bumped version to ' + context['release_version'] + '"'
-        ])
-        self.execute_command(context, 'git tag {}'.format(context['release_version']).split())
-        self.execute_command(context, 'git checkout {}'.format(context['new_stabilization_branch']).split())
-        # this is a transient branch/merge, so we won't actually merge, we'll just mark it as merged
-        self.execute_command(context, 'git merge --strategy=ours {}'.format(context['new_release_branch']).split())
-        self.bump_version_to(context, context['next_stable_version'])
-        self.execute_command(context, 'git add {}'.format(context['project_file']).split())
-        # this has spaces in a parameter, so it's different...
-        self.execute_command(context, [
-            'git',
-            'commit',
-            '-m',
-            '"bumped version to ' + context['next_stable_version'] + '"'
-        ])
-        self.execute_command(context, 'git checkout master'.split())
-        self.execute_command(context, 'git merge --strategy=ours {}'.format(context['new_stabilization_branch']).split())
-        self.bump_version_to(context, context['next_master_version'])
-        self.execute_command(context, 'git add {}'.format(context['project_file']).split())
-        # this has spaces in a parameter, so it's different...
-        self.execute_command(context, [
-            'git',
-            'commit',
-            '-m',
-            '"bumped version to ' + context['next_master_version'] + '"'
-        ])
-
-    def release_from_point(self, context):
-        self.execute_command(context, 'git checkout {}'.format(context['current_release_branch']).split())
-        self.execute_command(context, 'git merge {}'.format(context['current_branch']).split())
-        self.bump_version_to(context, context['release_version'])
-        self.execute_command(context, 'git add {}'.format(context['project_file']).split())
-        # this has spaces in a parameter, so it's different...
-        self.execute_command(context, [
-            'git',
-            'commit',
-            '-m',
-            '"bumped version to ' + context['release_version'] + '"'
-        ])
-        self.execute_command(context, 'git tag {}'.format(context['release_version']).split())
-        self.execute_command(context, 'git checkout {}'.format(context['current_branch']).split())
-        self.execute_command(context, 'git merge {}'.format(context['current_release_branch']).split())
-        self.bump_version_to(context, context['next_stable_version'])
-        self.execute_command(context, 'git add {}'.format(context['project_file']).split())
-        # this has spaces in a parameter, so it's different...
-        self.execute_command(context, [
-            'git',
-            'commit',
-            '-m',
-            '"bumped version to ' + context['next_stable_version'] + '"'
-        ])
+        for step in steps:
+            step.execute(context)
