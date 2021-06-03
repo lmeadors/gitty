2aa295d5b784506e87baa2ae0deae746c143140f
Larry Meadors <larry.meadors@dillonsoftware.com>
2021-06-03 11:48:24 -0600

    fix: making plugins work with multiple files
    fix: added sample plugin

--- gitty/gitty_command_plugins.py
+++ gitty/gitty_command_plugins.py
@@ -14,20 +14,24 @@ class GittyPlugins(GittyCommand):
         context["plugins"] = []
 
         # look in the home directory and current directory for plugins
-        self.register_plugin_at(Path.home().joinpath('.gitty'), context, 'home')
-        self.register_plugin_at(Path.cwd().joinpath(".gitty"), context, 'local')
+        self.register_plugins_at(Path.home().joinpath('.gitty'), context, 'home')
+        self.register_plugins_at(Path.cwd().joinpath(".gitty"), context, 'local')
 
     @staticmethod
-    def register_plugin_at(directory, context, suffix):
-        plugin_path = directory.joinpath('plugins.py')
-        module_name = 'gitty_plugin_{0}'.format(suffix)
+    def register_plugins_at(plugin_path, context, suffix):
+        # plugin_path = directory.joinpath('plugins.py')
         if plugin_path.exists():
             import imp
             # print('adding plugin from {0} as {1}'.format(plugin_path, module_name))
-            with open(plugin_path, 'rb') as fp:
-                plugin = imp.load_module(module_name, fp, '.gitty/plugins.py', ('.py', 'rb', imp.PY_SOURCE))
-                plugin.register(context)
-                context["plugins"].append(plugin.describe(context))
+            index = 0
+            for plugin in plugin_path.glob('*.py'):
+                module_name = 'gitty_plugin_{0}_{1}'.format(suffix, index)
+                index = index +1
+                with open(plugin, 'rb') as fp:
+                    print('loading module {0} from file {1}'.format(module_name, plugin))
+                    plugin = imp.load_module(module_name, fp, str(plugin), ('.py', 'rb', imp.PY_SOURCE))
+                    plugin.register(context)
+                    context["plugins"].append(plugin.describe(context))
 
     def do_it(self, context):
         # print('available commands on branch "{}" are:'.format(Color.white_lt(context['current_branch'])))
---
+++ plugin_samples/pull_request.py
@@ -0,0 +1,42 @@
+from gitty import GittyCommand, Color
+
+
+def register(context):
+	# we're installing a new command here - append it to the commands in the context
+	context["commands"].append(GittyNewPullRequest())
+
+
+def describe(context):
+	return 'pr - create a new AWS CodeCommit pull request'
+
+
+class GittyNewPullRequest(GittyCommand):
+	_title = 'new pull request'
+	_name = 'create a new pull request'
+	_bindings = ['pr']
+
+# 	def __init__(self, context):
+# 		print('creating new pr command')
+
+	def do_it(self, context):
+
+		cmd_for_remote_url = 'git remote get-url {0}'.format(context['git_remote'])
+		repo_name = GittyCommand.execute_command_safe(context, cmd_for_remote_url.split(' ')).decode('utf-8')
+		options = '--targets repositoryName={0},sourceReference={1},destinationReference={2}'.format(
+			repo_name,
+			context['current_branch'],
+			context['parent_version_branch'],
+		)
+
+		print(
+			'aws codecommit create-pull-request --no-cli-pager --title {0} {1} --query pullRequest.pullRequestId'.format(
+				context['branch_parts'][-1],
+				options
+			)
+		)
+
+		# RESPONSE=`eval "${CMD}"`
+		# RESPONSE="${RESPONSE%\"}"
+		# RESPONSE="${RESPONSE#\"}"
+		# echo "${RESPONSE}"
+		# echo "https://console.aws.amazon.com/codesuite/codecommit/repositories/${REPO}/pull-requests/${RESPONSE}/details?region=us-east-1"
